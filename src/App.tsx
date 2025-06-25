adb75814108584e45e4f27ff74575c77898175fb
fusioneventsak <arthurk@fusion-events.ca>
2025-06-25 12:59:32 -0400

    Implement React Router v7 Future Flag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+  Route, 
+  createRoutesFromElements 
+} from 'react-router-dom';
 
 // Pages
 import LandingPage from './pages/LandingPage';
@@ -12,27 +17,35 @@ import CollageModerationPage from './pages/CollageModerationPage';
 import PhotoboothPage from './pages/PhotoboothPage';
 import JoinCollage from './pages/JoinCollage';
 
+// Create router with future flag enabled
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      {/* Public routes */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/join" element={<JoinCollage />} />
+      <Route path="/collage/:code" element={<CollageViewerPage />} />
+      <Route path="/photobooth/:code" element={<PhotoboothPage />} />
+      
+      {/* Protected routes - require authentication */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard/collage/:id" element={<CollageEditorPage />} />
+      <Route path="/collage/:id/moderation" element={<CollageModerationPage />} />
+      <Route path="/moderation/:id" element={<CollageModerationPage />} />
+    </Route>
+  ),
+  {
+    future: {
+      v7_relativeSplatPath: true
+    }
+  }
+);
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/join" element={<JoinCollage />} />
-        <Route path="/collage/:code" element={<CollageViewerPage />} />
-        <Route path="/photobooth/:code" element={<PhotoboothPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        
-        {/* Protected routes - require authentication */}
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/dashboard/collage/:id" element={<CollageEditorPage />} />
-        <Route path="/collage/:id/moderation" element={<CollageModerationPage />} />
-        <Route path="/moderation/:id" element={<CollageModerationPage />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
 
